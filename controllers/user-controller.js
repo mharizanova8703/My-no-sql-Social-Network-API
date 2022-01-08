605017acf0c27136d9d624652560d1368aa7c2e3
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2022-01-08 09:09:38 -0500

    creating a user create

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -32,5 +32,10 @@ const userConroller = {
         res.status(500).json(err)
       })
   },
+  createUser({ body}, res) {
+      User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
 }
 module.exports = userController
