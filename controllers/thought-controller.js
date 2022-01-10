040a7c9bffe51b648c31c1733cad277888c1e275
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2022-01-10 18:23:45 -0500

    update

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,12 +3,7 @@ const { Thought, User } = require('../models')
 const thoughtController = {
   getAllThoughts(req, res) {
     Thought.find({})
-      //   .populate({
-      //     path: 'user',
-      //     select: '-__v',
-      //   })
       .select('-__v')
-      //   .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
         console.log(err)
